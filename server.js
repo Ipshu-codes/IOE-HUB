26944ee93d5aae8046e5e3866d679496683c9bee
gugu-codes <ipshu.upreti@gmail.com>
2026-07-20 20:58:42 +0545

    Add previous year questions page and improve routing

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
+// Serve static assets
 app.use("/static", express.static(path.join(__dirname, "static")));
 
-// Home page
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+// Serve HTML files
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
